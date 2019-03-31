ca85070ebc3d472b6f0d2eca6af560790200a55c
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-03-31 14:08:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const port = process.env.PORT || 443,
 bot.setWebHook(externalUrl + ':443/bot' + token);
 bot.on("message", (msg) => {
     const chatId = msg.chat.id;
-    if(msg.text=="hai"||msg.text=="Hai"
-    bot.sendMessage(chatId, 'Hello');
+    if(msg.text=="hai"||msg.text=="Hai")
+    {bot.sendMessage(chatId, 'Hello');}
 });
